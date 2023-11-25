267a2e5c2817304e86dc0d92ca5727259c0de6c4
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-25 17:21:11 +0100

    Pass a Lambda Expression as a Method Parameter

--- src/com/dgmf/lambdaexpressions/withlambdas/ShapeClient.java
+++ src/com/dgmf/lambdaexpressions/withlambdas/ShapeClient.java
@@ -1,12 +1,12 @@
 package com.dgmf.lambdaexpressions.withlambdas;
 
 import com.dgmf.lambdaexpressions.withoutlambdas.Shape;
-import com.dgmf.lambdaexpressions.withoutlambdas.impl.Circle;
-import com.dgmf.lambdaexpressions.withoutlambdas.impl.Rectangle;
 
 public class ShapeClient {
     public static void main(String[] args) {
         // Using Lambdas to implement the "Shape" Functional Interface
+        System.out.println("======= Using Lambdas to implement the \"Shape\" " +
+                "Functional Interface =======");
         Shape circle = () -> System.out.println("Circle Class : draw() Method");
         circle.draw();
 
@@ -16,5 +16,17 @@ public class ShapeClient {
         Shape square = () -> System.out.println("Square Class : draw() Method");
         square.draw();
 
+        // Pass a Lambda Expression as a Method Parameter
+        System.out.println("======= Pass a Lambda Expression as a Method " +
+                "Parameter =======");
+        print(circle);
+        print(rectangle);
+        print(square);
+
+    }
+
+    // Pass a Lambda Expression as a Method Parameter
+    public static void print(Shape shape) {
+        shape.draw();
     }
 }
