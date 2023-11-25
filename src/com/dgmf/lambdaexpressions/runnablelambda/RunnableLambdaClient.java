5fad3add3d33d19953375f857bf2e89504847833
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-25 19:01:02 +0100

    Using Lambda Expression to create a Thread Object

--- src/com/dgmf/lambdaexpressions/runnablelambda/RunnableLambdaClient.java
+++ src/com/dgmf/lambdaexpressions/runnablelambda/RunnableLambdaClient.java
@@ -3,9 +3,16 @@ package com.dgmf.lambdaexpressions.runnablelambda;
 public class RunnableLambdaClient {
     public static void main(String[] args) {
         // Traditional way to create a Thread Object
-        System.out.println("====== Traditional way to create a Thread Object ======");
         Thread thread = new Thread(new LambdaThread());
         thread.start();
 
+        // Using Lambda Expression to create a Thread Object
+        // Runnable runnable = () -> System.out.println("Runnable run() Method called " +
+        //         "using Lambda Expression ...");
+        // Thread lambdaThread = new Thread(runnable);
+        // Thread lambdaThread = new Thread(runnable);
+        Thread lambdaThread = new Thread(() -> System.out.println("Runnable run() Method called " +
+                "using Lambda Expression ..."));
+        lambdaThread.start();
     }
 }
