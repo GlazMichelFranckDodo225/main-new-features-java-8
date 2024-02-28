6be57247c7e49a2d5b261145f28883c3e8805e6b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-28 05:42:03 +0100

    Create a Stream from Existing Sources

--- src/com/dgmf/e/streamapi/CreateStreamObjects.java
+++ src/com/dgmf/e/streamapi/CreateStreamObjects.java
@@ -1,4 +1,5 @@
 package com.dgmf.e.streamapi;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class CreateStreamObjects {
@@ -7,5 +8,28 @@ public class CreateStreamObjects {
         Stream<String> stringStream = Stream.of("a", "b", "c");
         // Perform "forEach" Method
         stringStream.forEach(System.out::println);
+
+        // Create a Stream from Existing Sources
+        // A Stream from a Collection
+        Collection<String> collection =
+                Arrays.asList("Java", "Jakarta", "Spring", "Hibernate");
+        Stream<String> collectionStream = collection.stream();
+        collectionStream.forEach(System.out::println);
+
+        // A Stream from a List
+        List<String> list =
+                Arrays.asList("Mercedes", "Ferrari", "BMW");
+        Stream<String> listStream = list.stream();
+        listStream.forEach(System.out::println);
+
+        // A Stream from a Set
+        Set<String> set = new HashSet<>(list);
+        Stream<String> setStream = set.stream();
+        setStream.forEach(System.out::println);
+
+        // A Stream from a Array
+        String[] array = {"John Doe", "Manuela De Lalorga", "Vixente Lizzarazu"};
+        Stream<String> arrayStream = Arrays.stream(array);
+        arrayStream.forEach(System.out::println);
     }
 }
