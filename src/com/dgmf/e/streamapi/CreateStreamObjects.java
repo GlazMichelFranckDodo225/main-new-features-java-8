a25662581d897e621843ab075229b65e407f7caf
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-28 05:20:55 +0100

    Create a Stream - V2

--- src/com/dgmf/e/streamapi/CreateStreamObjects.java
+++ src/com/dgmf/e/streamapi/CreateStreamObjects.java
@@ -1,13 +1,11 @@
 package com.dgmf.e.streamapi;
-
 import java.util.stream.Stream;
 
 public class CreateStreamObjects {
     public static void main(String[] args) {
         // Create a Stream
-        Stream<String> stream = Stream.of("a", "b", "c");
-
+        Stream<String> stringStream = Stream.of("a", "b", "c");
         // Perform "forEach" Method
-        stream.forEach(System.out::println);
+        stringStream.forEach(System.out::println);
     }
 }
