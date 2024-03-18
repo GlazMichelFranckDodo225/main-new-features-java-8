953dd5140b1a51f6e1276ee03e0bbf658b8e443d
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-18 15:43:44 +0100

    Stream API : Sort in Descending Order

---
+++ src/com/dgmf/e/streamapi/sorting/StreamListSortingDescendingOrder.java
@@ -0,0 +1,33 @@
+package com.dgmf.e.streamapi.sorting;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamListSortingDescendingOrder {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Mango");
+        fruits.add("Orange");
+
+        // Stream API : Sort in Descending Order
+        System.out.println(fruits); // [Banana, Apple, Mango, Orange]
+
+        // Reverse Order
+        List<String> reverseSortedList = fruits.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        System.out.println(reverseSortedList);
+
+        // Using Lambda Expressions
+        List<String> lambdaSortedList = fruits.stream()
+                .sorted((o1, o2) -> o2.compareTo(o1))
+                .collect(Collectors.toList());
+
+        System.out.println(lambdaSortedList);
+    }
+}
