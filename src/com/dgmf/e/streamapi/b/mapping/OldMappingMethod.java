33c72feb853b94948e1ca1c301594854d413eac1
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-04 14:28:35 +0200

    Converting Users to UserDtos Using the "map()" Method of the Stream API

--- src/com/dgmf/e/streamapi/b/mapping/OldMappingMethod.java
+++ src/com/dgmf/e/streamapi/b/mapping/OldMappingMethod.java
@@ -2,11 +2,11 @@ package com.dgmf.e.streamapi.b.mapping;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class StreamMapCollect {
+public class OldMappingMethod {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
+        List<UserDto> userDtos = new ArrayList<>();
 
         users.add(new User(
                 1,
@@ -27,18 +27,18 @@ public class StreamMapCollect {
                 "adebayorkobenan@gmail.com")
         );
 
-        // Converting Users to UserDtos Using the "map()"
-        // Method of the Stream API
-        List<UserDto> userDtos = users.stream()
-                .map((User user) -> new UserDto(
-                                user.getId(),
-                                user.getUserName(),
-                                user.getEmail()
-                        )
-                )
-                // .forEach(System.out::println); ==> Without Storage
-                .collect(Collectors.toList()); // ==> With Storage
+        // Conversion of Users to UserDtos without the
+        // "map()" Method of the Stream API
+        for(User user: users) {
+            UserDto userDto = new UserDto(
+                    user.getId(), user.getUserName(), user.getEmail()
+            );
+            userDtos.add(userDto);
+        }
 
-        userDtos.forEach(System.out::println);
+        // Outputs
+        for(UserDto userDto: userDtos) {
+            System.out.println(userDto);
+        }
     }
 }
